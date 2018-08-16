67633ba193af91e911ea299b6b4f6178d40a4616
WhiteDogDESKTOP <wodnr97@gmail.com>
2018-08-16 19:12:24 +0900

    오브젝트를 구분할 수 있는 Group을 추가한다
    IGroupable 인터페이스를 통해 Group을 나눌 수 있게 한다.

---
+++ Assets/Scripts/Group.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity;
+
+
+public class Group : MonoBehaviour {
+    public enum KIND
+    {
+        VOXELLERS = 0,
+        INVADERS = 1,
+        NEUTURAL = 2,
+        ITEM = 3,
+    }
+
+    [SerializeField]
+    private KIND _kind;
+    public KIND Kind
+    {
+        set { _kind = value; }
+        get { return _kind; }
+    }
+
+    /*
+     * -1 적대 0 중립 1 아군
+     */
+    private int[,] _relationship = new int[4, 4]
+        {{1, -1, 0, 0 },
+            {-1, 1, 0, 0 },
+            {0, 0, 0, 0 },
+            {0, 0, 0, 0 } };
+    public int[,] Relationship {
+        get { return _relationship; }
+            }
+}
