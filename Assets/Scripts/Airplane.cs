fcd3716e4982589aa1012b22232251611aa2a204
WhiteDogDESKTOP <wodnr97@gmail.com>
2018-08-17 18:46:34 +0900

    제목
    Airplane 스크립트를 만들어 IDamageable의 함수를 구현하게한다.
    Airplane에 Health를 두어, Bullet과 충돌하였을 때 소멸되는 동작의
    테스트가 가능하도록 임시적으로 만들었다.
    
    IComparable 인터페이스는 그 이용목적이 명확치 않아 삭제한다.
    CompareTo 함수는 이제 Bullet 스크립트의 전유물이 된다.
    
    NalBullet은 IDamageable 인터페이스를 가지고 있는 대상이라면,
    그것의 TakeDamage 함수를 호출하는 형식으로 만든다.

---
+++ Assets/Scripts/Airplane.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airplane : MonoBehaviour, IDamageable {
+    [SerializeField]
+    float _health = 100f;
+    public float Health
+    {
+        set
+        {   
+            _health = value;
+            if (_health <= 0)
+                Destroy(gameObject);
+        }
+        get
+        {
+            return _health;
+        }
+    }
+
+    public void TakeDamage(float amount)
+    {
+        Health -= amount;
+    }
+}
