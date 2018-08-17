7cc39857201b190a5df23f25c0e39dc1d05090e5
WhiteDogDESKTOP <wodnr97@gmail.com>
2018-08-18 01:50:30 +0900

    Gun을 제어하는 GunController를 추가한다
    Gun을 쏘게하고, 장전하게 명령하는 GunController 스크립트를 추가한다.
    
    GunController에 그것이 AI든 플레이어든 공통적으로 사용해야할 필드와
    함수인 재장전 시간과, 컨트롤할 Gun들을 모은 리스트, Gun을 작동시키는
    형상의 함수 PullTrigger와 장전 함수를 추가한다.
    
    PlayerController를 추가하여 플레이어의 마우스 1,2 번 인풋을 받아
    사격과 장전을 처리하게 한다.

---
+++ Assets/GunController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+    public List<Gun> gunsToControl = new List<Gun>();
+    public float reloadTime;
+
+    protected virtual void PullTrigger()
+    {
+        for(int i = 0; i<gunsToControl.Count; i++)
+        {
+            gunsToControl[i].StartCoroutine("OnPullTrigger");
+        }
+    }
+
+    protected virtual IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(reloadTime);
+
+        for (int i = 0; i < gunsToControl.Count; i++)
+        {
+            gunsToControl[i].currentLoadedAmmoAmount = gunsToControl[i].maxLoadedAmmoAmount;
+        }
+
+        yield return null;
+    }
+}
---
+++ Assets/PlayerGunController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGunController : GunController {
+    // Update is called once per frame
+    protected void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            PullTrigger();
+        }
+        if(Input.GetButtonDown("Fire2"))
+        {
+            StartCoroutine("Reload");
+        }
+    }
+}
